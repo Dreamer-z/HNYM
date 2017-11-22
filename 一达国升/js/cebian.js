9c71d6c06cfa2bc9bb070da8783143bfc77a4725
Dreamer <974477966@qq.com>
2017-11-22 08:33:59 +0800

    hnym

---
+++ 一达国升/js/cebian.js
@@ -0,0 +1,15 @@
+ $(".brow").mousemove(function () {
+        $(".product").show();
+    });
+    $(".brow").mouseleave(function () {
+        $(".product").hide();
+    });
+	 $(".ce").mousemove(function () {
+        $(".product").show();
+    });
+    $(".ce").mouseleave(function () {
+        $(".product").hide();
+    });
+	 $(".ce").hover(function () {
+        $(".product").show();
+    });
